efecaf48aec76776c8c431d3a6610400ce001251
Tuhin Al Rakib <eng.tuhin77@gmail.com>
2025-01-19 20:24:04 +0600

    2nd

--- app.js
+++ app.js
@@ -8,12 +8,13 @@ const displayProducts = (products)=>{
     const productContainer = document.getElementById("product-container")
 
     products.forEach(product=>{
+        console.log(product)
         const div = document.createElement("div")
         // div.classList.add("")
         div.innerHTML = `
-        <img src="" alt="" />
-        <h5></h5>
-        <p></p>
+        <img class="card-img" src="${product.image}" alt="" />
+        <h5> ${product.title.slice(0,10)} </h5>
+        <p> ${product.description.slice(0,20)} </p>
         <button>Details</button>
         <button>Add To Cart</button>
         `
